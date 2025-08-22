192c97d3d14a9c56e714968767da5c47bfec5dd8
Joshua-Samraj <j.joshuasamraj@gmail.com>
2025-08-22 11:40:09 +0530

    add one more image

--- src/data/galleriesData.js
+++ src/data/galleriesData.js
@@ -542,6 +542,15 @@ export const GALLERIES_DATA = {
         caption: "Cancer Survivors Day 2",
         description: "Inspiring stories from cancer survivors"
       },
+      {
+        id: "day2_021",
+        url: "/assets/Day 2/(21).jpg",
+        src: "",
+        alt: "Cancer Survivors Day 2 event 3",
+        caption: "Cancer Survivors Day 2",
+        description: "Inspiring stories from cancer survivors"
+      },
+      
     ]
   },
   community: {
